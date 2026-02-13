ca6f00c97bd7b3eac6b563ae06c41cc46b099438
GFC <chenguanfu4@gmail.com>
2026-02-13 11:42:45 +0800

    260213_Activity_V2_編輯 Controller 內容

--- TravelWeb/Areas/Activity/Controllers/ActivityController.cs
+++ TravelWeb/Areas/Activity/Controllers/ActivityController.cs
@@ -6,10 +6,56 @@ namespace TravelWeb.Areas.Activity.Controllers
     [Route("Act")]
     public class ActivityController : Controller
     {
-        [HttpGet("Home/{num?}")]
-        public IActionResult Index(int num)
+
+        //活動總覽
+        [HttpGet("Overview")]
+        public IActionResult Index()
         {
-            return Content($"路由來的: {num}");
+
+            return View();
         }
+
+        //活動內容設定
+        [HttpGet("Edit")]
+        public IActionResult ActivityEdit() 
+        {
+            return View();
+        }
+
+        [HttpPost("ActivityEdit")]
+        public IActionResult ActivityEdit(int a) 
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        //活動票卷設定
+        [HttpGet("Ticket")]
+        public IActionResult TicketManage() 
+        {
+            return View();
+        }
+
+        [HttpPost("TicketEdit")]
+        public IActionResult TicketManage(int a) 
+        {
+            return RedirectToAction(nameof(TicketManage));
+        }
+
+        // 活動排程設定
+        [HttpGet("Schedule")]
+        public IActionResult ActivitySetUp() 
+        {
+            return View();
+        }
+
+        [HttpPost("Schedule")]
+        public IActionResult ActivitySetUp(int a) 
+        {
+            return RedirectToAction(nameof(ActivitySetUp));
+        }
+
+        //活動熱度分析
+
+
     }
 }
