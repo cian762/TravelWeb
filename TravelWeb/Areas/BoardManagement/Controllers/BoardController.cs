8bae3af5efbbb9cb1f1a984ec950bcc8d24ce3c7
cian762 <cian484848@gmail.com>
2026-02-17 20:50:07 +0800

    0217合併Main版

--- TravelWeb/Areas/BoardManagement/Controllers/BoardController.cs
+++ TravelWeb/Areas/BoardManagement/Controllers/BoardController.cs
@@ -16,7 +16,7 @@ namespace TravelWeb.Areas.BoardManagement.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(); 
         }
 
         public IActionResult ReportManager()
