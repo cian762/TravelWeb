bd826ece7991bae0a34d3196de8acf1089612466
Frank Chen <chenguanfu4@gmail.com>
2026-03-06 15:11:30 +0800

    260306_v2_Activity Ticket 分層架構拆解_完成

--- TravelWeb/Areas/Activity/Repository/IActivityRepositories/IActivityTicketReposiotry.cs
+++ TravelWeb/Areas/Activity/Repository/IActivityRepositories/IActivityTicketReposiotry.cs
@@ -1,4 +1,5 @@
 ﻿using CloudinaryDotNet.Actions;
+using TravelWeb.Areas.Activity.Models.EFModel;
 using TravelWeb.Areas.Activity.Models.ViewModels;
 
 namespace TravelWeb.Areas.Activity.Repository.IActivityRepositories
@@ -9,11 +10,16 @@ namespace TravelWeb.Areas.Activity.Repository.IActivityRepositories
 
         Task<ActivityTicketViewModel> GetByProductCodeAsync(string ProductCode);
 
+        Task<List<AcitivityTicket>> GetByActivityIdAsync(int activityId);
+
         Task CreateAsync(ActivityTicketViewModel vm);
 
-        Task UpdateAsync(ActivityTicketViewModel vm);
+        Task UpdateAsync(string ProductCode, ActivityTicketViewModel vm);
 
         Task SaveChangeAsync();
 
+
+
+        Task<List<string>> TakeTicketCategoryNames();
     }
 }
